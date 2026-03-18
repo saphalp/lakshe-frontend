a07c5a8318988758f4a6e1a7b4543dbb44f48e49
saphalp <saphalpant2003@gmail.com>
2026-03-18 00:08:33 -0500

    fix: add renders to login and signup page

--- lakshe/components/ui/Navbar.tsx
+++ lakshe/components/ui/Navbar.tsx
@@ -17,8 +17,8 @@ export default function Navbar() {
 
   return (
     <>
-    <nav className="flex justify-between px-15 py-4 font-sans sticky items-center text-md">
-      <Link href='/' className="font-bold text-white text-xl"> Lakshe </Link>
+    <nav className="flex justify-between py-4 px-20 font-sans sticky items-center text-md">
+      <Link href='/' className="font-bold text-white text-xl w-60"> Lakshe </Link>
       <ul className="flex gap-10 text-[oklch(55.4%_0.046_257.417)] items-center">
         {links.map(({ href, label }) => (
           <li key={href}>
@@ -26,9 +26,9 @@ export default function Navbar() {
           </li>
         ))}
       </ul>
-      <div className="flex gap-7">
-        <button className="text-white"> Log in </button>
-        <button className="bg-emerald-400 w-30 h-10 text-white rounded-sm"> Get Started </button>
+      <div className="flex gap-7 w-60">
+        <Link href='/login'><button className="w-15 h-10 text-white"> Log in </button></Link>
+        <Link href='/sign-up'><button className="bg-emerald-400 w-30 h-10 text-white rounded-sm"> Get Started </button></Link>
       </div>
     </nav>
     <hr className="border-[oklch(55.4%_0.046_257.417)]" />
