de5c0dfaa18886e3506c9f707c02fac89c04da5f
saphalp <saphalpant2003@gmail.com>
2026-03-22 17:37:18 -0500

    feat: form validation for all forms

--- lakshe/components/ui/EducationForm.tsx
+++ lakshe/components/ui/EducationForm.tsx
@@ -1,12 +1,12 @@
-"use client"
+"use client";
 
-import React, { useState } from "react"
-import { useStep } from "@/context/StepContext"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Card, CardContent } from "./card"
-import { Group, X } from "lucide-react"
+import React, { useState } from "react";
+import { useStep } from "@/context/StepContext";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Card, CardContent } from "./card";
+import { X } from "lucide-react";
 import {
   Item,
   ItemActions,
@@ -14,147 +14,198 @@ import {
   ItemDescription,
   ItemMedia,
   ItemTitle,
-} from "@/components/ui/item"
-import { useFormData } from "@/context/FormContext"
+} from "@/components/ui/item";
+import { useFormData } from "@/context/FormContext";
+import { useForm, SubmitHandler } from "react-hook-form";
 
+type Inputs = {
+  educations: EducationEntry[];
+};
+
+type EducationEntry = {
+  institution: string;
+  degree: string;
+  field: string;
+  startYear: string;
+  endYear: string;
+};
 
 export default function EducationForm() {
-  const { nextStep, prevStep } = useStep()
-  const { formData, updateFormData } = useFormData()
-  const [educations, setEducations] = useState<EducationEntry[]>(formData.education)
+  const { nextStep, prevStep } = useStep();
+  const { formData, updateFormData } = useFormData();
+  const [educations, setEducations] = useState<EducationEntry[]>(
+    formData.education,
+  );
 
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: {
+      educations: formData.education,
+    },
+  });
 
-
-  type EducationEntry = {
-    institution: string
-    degree: string
-    field: string
-    startYear: string
-    endYear: string
-  }
-
-    const [form, setForm] = useState<EducationEntry>({
+  const [form, setForm] = useState<EducationEntry>({
     institution: "",
     degree: "",
     field: "",
     startYear: "",
     endYear: "",
-    })
+  });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }))
-  }
+    setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    updateFormData({education: educations})
-    // TODO: validate & persist form data
-    nextStep()
-  }
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    handleAdd();
+    updateFormData({ education: data.educations });
+    nextStep();
+  };
 
-const handleAdd = (e: React.MouseEvent<HTMLButtonElement>) => {
-  setEducations((prev) => [...prev, form])
-  setForm({ institution: "", degree: "", field: "", startYear: "", endYear: "" })
-}
+  const handleAdd = () => {
+    if (!form.institution && !form.degree) return;
+    const updated = [...educations, form];
+    setEducations(updated);
+    setValue("educations", updated);
+    setForm({
+      institution: "",
+      degree: "",
+      field: "",
+      startYear: "",
+      endYear: "",
+    });
+  };
 
-const removeEducation = (education: EducationEntry) => {
-    setEducations((prev) => prev.filter((s) => s !== education))
-}
+  const removeEducation = (education: EducationEntry) => {
+    const updated = educations.filter((s) => s !== education);
+    setEducations(updated);
+    setValue("educations", updated);
+  };
 
   return (
     <>
-    <div className="flex flex-col gap-4 mb-6">
-    {educations.map((item, index) => (
-    <Item variant='default' className="bg-card" key={index}>
-        <ItemContent>
-          <ItemTitle>{item.institution}</ItemTitle>
-          <ItemDescription>
-            {item.degree} ({item.field}) {item.startYear}-{item.endYear}
-          </ItemDescription>
-        </ItemContent>
-        <ItemActions>
-          <button
+      <div className="flex flex-col gap-4 mb-6">
+        {educations.map((item, index) => (
+          <Item variant="default" className="bg-card" key={index}>
+            <ItemContent>
+              <ItemTitle>{item.institution}</ItemTitle>
+              <ItemDescription>
+                {item.degree} ({item.field}) {item.startYear}-{item.endYear}
+              </ItemDescription>
+            </ItemContent>
+            <ItemActions>
+              <button
                 type="button"
                 onClick={() => removeEducation(item)}
                 className="hover:text-white transition-colors"
               >
                 <X className="w-6 h-6" />
-        </button>
-        </ItemActions>
-    </Item>
-    ))}
-    </div>
-    <Card>
-    <CardContent className="text-gray-400">
-    <form onSubmit={handleSubmit} className="flex flex-col gap-5">
-      <div className="flex flex-col gap-1.5">
-        <Label className='text-xs font-bold' htmlFor="institution">INSTITUTION</Label>
-        <Input
-          id="institution"
-          name="institution"
-          placeholder="e.g. MIT"
-          value={form.institution}
-          onChange={handleChange}
-        />
+              </button>
+            </ItemActions>
+          </Item>
+        ))}
       </div>
+      <Card>
+        <CardContent className="text-gray-400">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-5"
+          >
+            <div className="flex flex-col gap-1.5">
+              <Label className="text-xs font-bold" htmlFor="institution">
+                INSTITUTION
+              </Label>
+              <Input
+                id="institution"
+                name="institution"
+                placeholder="e.g. MIT"
+                value={form.institution}
+                onChange={handleChange}
+              />
+            </div>
 
-      <div className="flex flex-col gap-1.5">
-        <Label htmlFor="degree" className='text-xs font-bold'>DEGREE</Label>
-        <Input
-          id="degree"
-          name="degree"
-          placeholder="e.g. Bachelor of Science"
-          value={form.degree}
-          onChange={handleChange}
-        />
-      </div>
+            <div className="flex flex-col gap-1.5">
+              <Label htmlFor="degree" className="text-xs font-bold">
+                DEGREE
+              </Label>
+              <Input
+                id="degree"
+                name="degree"
+                placeholder="e.g. Bachelor of Science"
+                value={form.degree}
+                onChange={handleChange}
+              />
+            </div>
 
-      <div className="flex flex-col gap-1.5">
-        <Label htmlFor="field" className='text-xs font-bold'>FIELD OF STUDY</Label>
-        <Input
-          id="field"
-          name="field"
-          placeholder="e.g. Computer Science"
-          value={form.field}
-          onChange={handleChange}
-        />
-      </div>
+            <div className="flex flex-col gap-1.5">
+              <Label htmlFor="field" className="text-xs font-bold">
+                FIELD OF STUDY
+              </Label>
+              <Input
+                id="field"
+                name="field"
+                placeholder="e.g. Computer Science"
+                value={form.field}
+                onChange={handleChange}
+              />
+            </div>
 
-      <div className="flex gap-4">
-        <div className="flex flex-col gap-1.5 flex-1">
-          <Label htmlFor="startYear" className='text-xs font-bold'>START YEAR</Label>
-          <Input
-            id="startYear"
-            name="startYear"
-            placeholder="2018"
-            value={form.startYear}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex flex-col gap-1.5 flex-1">
-          <Label htmlFor="endYear" className='text-xs font-bold'>END YEAR</Label>
-          <Input
-            id="endYear"
-            name="endYear"
-            placeholder="2022"
-            value={form.endYear}
-            onChange={handleChange}
-          />
-        </div>
-      </div>
+            <div className="flex gap-4">
+              <div className="flex flex-col gap-1.5 flex-1">
+                <Label htmlFor="startYear" className="text-xs font-bold">
+                  START YEAR
+                </Label>
+                <Input
+                  id="startYear"
+                  name="startYear"
+                  placeholder="2018"
+                  value={form.startYear}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex flex-col gap-1.5 flex-1">
+                <Label htmlFor="endYear" className="text-xs font-bold">
+                  END YEAR
+                </Label>
+                <Input
+                  id="endYear"
+                  name="endYear"
+                  placeholder="2022"
+                  value={form.endYear}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
 
-      <div className="flex justify-between mt-2">
-        <Button type="button" variant="outline" onClick={prevStep} className="w-30 bg-transparent">
-          Back
-        </Button>
-        <div>
-        <Button onClick={handleAdd} type="button" className="w-30 bg-indigo-700 mr-3">Add another</Button>
-        <Button type="submit" className="w-30">Continue</Button>
-        </div>
-      </div>
-    </form>
-    </CardContent>
-    </Card>
+            <div className="flex justify-between mt-2">
+              <Button
+                type="button"
+                variant="outline"
+                onClick={prevStep}
+                className="w-30 bg-transparent"
+              >
+                Back
+              </Button>
+              <div>
+                <Button
+                  onClick={handleAdd}
+                  type="button"
+                  className="w-30 bg-indigo-700 mr-3"
+                >
+                  Add
+                </Button>
+                <Button type="submit" className="w-30">
+                  Continue
+                </Button>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
     </>
-  )
+  );
 }
--- lakshe/components/ui/ExperienceForm.tsx
+++ lakshe/components/ui/ExperienceForm.tsx
@@ -1,12 +1,12 @@
-"use client"
+"use client";
 
-import React, { useState } from "react"
-import { useStep } from "@/context/StepContext"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Textarea } from "@/components/ui/textarea"
-import { Card, CardContent } from "./card"
+import React, { useState } from "react";
+import { useStep } from "@/context/StepContext";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { Card, CardContent } from "./card";
 import {
   Item,
   ItemActions,
@@ -14,156 +14,205 @@ import {
   ItemDescription,
   ItemMedia,
   ItemTitle,
-} from "@/components/ui/item"
-import { X } from "lucide-react"
-import { useFormData } from "@/context/FormContext"
-
-
-
-
-export default function ExperienceForm() {
-  const { prevStep } = useStep()
-  const { formData, updateFormData } = useFormData()
-  const [experiences, setExperiences] = useState<ExperiencesEntry[]>(formData.experiences)
+} from "@/components/ui/item";
+import { X } from "lucide-react";
+import { useFormData } from "@/context/FormContext";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 type ExperiencesEntry = {
-  company: string
-  role: string
-  startDate: string
-  endDate: string
-  description: string
-}
+  company: string;
+  role: string;
+  startDate: string;
+  endDate: string;
+  description: string;
+};
 
-const [form, setForm] = useState<ExperiencesEntry>({
-  company: "",
-  role: "",
-  startDate: "",
-  endDate: "",
-  description: "",
-})
+type Inputs = {
+  experiences: ExperiencesEntry[];
+};
+
+export default function ExperienceForm() {
+  const { formData, updateFormData } = useFormData();
+  const { prevStep } = useStep();
+  const [experiences, setExperiences] = useState<ExperiencesEntry[]>(
+    formData.experiences,
+  );
+
+  const [form, setForm] = useState<ExperiencesEntry>({
+    company: "",
+    role: "",
+    startDate: "",
+    endDate: "",
+    description: "",
+  });
+
+  const {
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: {
+      experiences: formData.experiences,
+    },
+  });
 
   const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => {
-    setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }))
-  }
+    setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const updated = [...experiences, form]
-    updateFormData({ experiences: updated })
-    //validation and submit logic
-    console.log(formData)
-  }
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    // const updated = [...experiences, form];
+    updateFormData({ experiences: data.experiences });
+    console.log(data);
+  };
 
   const handleAdd = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const updated = [...experiences, form]
-    setExperiences((prev) => [...prev, form])
-    setForm({ company: "", role: "", startDate: "", endDate: "", description: "" })
-    updateFormData({experiences: updated})
-  }
+    if (!form.company && !form.role) return;
+    const updated = [...experiences, form];
+    setExperiences(updated);
+    setValue("experiences", updated);
+    setForm({
+      company: "",
+      role: "",
+      startDate: "",
+      endDate: "",
+      description: "",
+    });
+    updateFormData({ experiences: updated });
+  };
 
   const removeExperience = (experience: ExperiencesEntry) => {
-    setExperiences((prev) => prev.filter((s) => s !== experience))
-  }
+    const updated = experiences.filter((s) => s !== experience);
+    setExperiences(updated);
+    setValue("experiences", updated);
+  };
 
   return (
     <>
-    <div className="flex flex-col gap-4 mb-6">
-    {experiences.map((item, index) => (
-    <Item variant='default' className="bg-card" key={index}>
-        <ItemContent>
-          <ItemTitle>{item.company}</ItemTitle>
-          <ItemDescription>
-            {item.role} - ({item.description}) {item.startDate}-{item.endDate}
-          </ItemDescription>
-        </ItemContent>
-        <ItemActions>
-          <button
+      <div className="flex flex-col gap-4 mb-6">
+        {experiences.map((item, index) => (
+          <Item variant="default" className="bg-card" key={index}>
+            <ItemContent>
+              <ItemTitle>{item.company}</ItemTitle>
+              <ItemDescription>
+                {item.role} - ({item.description}) {item.startDate}-
+                {item.endDate}
+              </ItemDescription>
+            </ItemContent>
+            <ItemActions>
+              <button
                 type="button"
                 onClick={() => removeExperience(item)}
                 className="hover:text-white transition-colors"
               >
                 <X className="w-6 h-6" />
-            </button>
-        </ItemActions>
-    </Item>
-    ))}
-    </div>
-    <Card>
-    <CardContent className="text-gray-400">
-    <form onSubmit={handleSubmit} className="flex flex-col gap-5">
-      <div className="flex flex-col gap-1.5">
-        <Label htmlFor="company" className='text-xs font-bold'>COMPANY</Label>
-        <Input
-          id="company"
-          name="company"
-          placeholder="e.g. Acme Corp"
-          value={form.company}
-          onChange={handleChange}
-        />
+              </button>
+            </ItemActions>
+          </Item>
+        ))}
       </div>
+      <Card>
+        <CardContent className="text-gray-400">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-5"
+          >
+            <div className="flex flex-col gap-1.5">
+              <Label htmlFor="company" className="text-xs font-bold">
+                COMPANY
+              </Label>
+              <Input
+                id="company"
+                name="company"
+                placeholder="e.g. Acme Corp"
+                value={form.company}
+                onChange={handleChange}
+              />
+            </div>
 
-      <div className="flex flex-col gap-1.5">
-        <Label htmlFor="role" className='text-xs font-bold'>ROLE</Label>
-        <Input
-          id="role"
-          name="role"
-          placeholder="e.g. Software Engineer"
-          value={form.role}
-          onChange={handleChange}
-        />
-      </div>
+            <div className="flex flex-col gap-1.5">
+              <Label htmlFor="role" className="text-xs font-bold">
+                ROLE
+              </Label>
+              <Input
+                id="role"
+                name="role"
+                placeholder="e.g. Software Engineer"
+                value={form.role}
+                onChange={handleChange}
+              />
+            </div>
 
-      <div className="flex gap-4">
-        <div className="flex flex-col gap-1.5 flex-1">
-          <Label htmlFor="startDate" className='text-xs font-bold'>START DATE</Label>
-          <Input
-            id="startDate"
-            name="startDate"
-            type="month"
-            value={form.startDate}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex flex-col gap-1.5 flex-1">
-          <Label htmlFor="endDate" className='text-xs font-bold'>END DATE</Label>
-          <Input
-            id="endDate"
-            name="endDate"
-            type="month"
-            value={form.endDate}
-            onChange={handleChange}
-            placeholder="Leave blank if current"
-            className="color-gray-400"
-          />
-        </div>
-      </div>
+            <div className="flex gap-4">
+              <div className="flex flex-col gap-1.5 flex-1">
+                <Label htmlFor="startDate" className="text-xs font-bold">
+                  START DATE
+                </Label>
+                <Input
+                  id="startDate"
+                  name="startDate"
+                  type="month"
+                  value={form.startDate}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="flex flex-col gap-1.5 flex-1">
+                <Label htmlFor="endDate" className="text-xs font-bold">
+                  END DATE
+                </Label>
+                <Input
+                  id="endDate"
+                  name="endDate"
+                  type="month"
+                  value={form.endDate}
+                  onChange={handleChange}
+                  placeholder="Leave blank if current"
+                  className="color-gray-400"
+                />
+              </div>
+            </div>
 
-      <div className="flex flex-col gap-1.5">
-        <Label htmlFor="description" className='text-xs font-bold'>DESCRIPTION</Label>
-        <Textarea
-          id="description"
-          name="description"
-          placeholder="Brief summary of your responsibilities and achievements"
-          value={form.description}
-          onChange={handleChange}
-          rows={4}
-        />
-      </div>
+            <div className="flex flex-col gap-1.5">
+              <Label htmlFor="description" className="text-xs font-bold">
+                DESCRIPTION
+              </Label>
+              <Textarea
+                id="description"
+                name="description"
+                placeholder="Brief summary of your responsibilities and achievements"
+                value={form.description}
+                onChange={handleChange}
+                rows={4}
+              />
+            </div>
 
-      <div className="flex justify-between mt-2">
-        <Button type="button" variant="outline" onClick={prevStep} className="w-30 bg-transparent">
-          Back
-        </Button>
-        <div>
-            <Button onClick={handleAdd} type="button" className="w-30 bg-indigo-700 mr-3">Add another</Button>
-            <Button type="submit" className="w-30">Finish</Button>
-        </div>
-      </div>
-    </form>
-    </CardContent>
-    </Card>
+            <div className="flex justify-between mt-2">
+              <Button
+                type="button"
+                variant="outline"
+                onClick={prevStep}
+                className="w-30 bg-transparent"
+              >
+                Back
+              </Button>
+              <div>
+                <Button
+                  onClick={handleAdd}
+                  type="button"
+                  className="w-30 bg-indigo-700 mr-3"
+                >
+                  Add
+                </Button>
+                <Button type="submit" className="w-30">
+                  Finish
+                </Button>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
     </>
-  )
+  );
 }
